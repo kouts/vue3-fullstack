245a94e34739307962a89c3df4ca9ae729c932e2
kouts <koutsaftakis@gmail.com>
2020-09-20 21:10:56 +0300

    Added loading card

---
+++ src/components/CardLoading.vue
@@ -0,0 +1,49 @@
+<template>
+  <div v-for="card in [1, 2]" :key="card" class="card" v-bind="$attrs">
+    <div class="card-body">
+      <div class="shimmer-bg" :style="shimmerStyle"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    shimmerStyle: {
+      type: Object,
+      default: () => ({
+        height: '100px'
+      })
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.shimmer-bg {
+  animation-duration: 2.2s;
+  animation-fill-mode: forwards;
+  animation-iteration-count: infinite;
+  animation-name: shimmer;
+  animation-timing-function: linear;
+  background: #ddd;
+  background: linear-gradient(to right, #f6f6f6 8%, #f0f0f0 18%, #f6f6f6 33%);
+  background-size: 1200px 100%;
+}
+@-webkit-keyframes shimmer {
+  0% {
+    background-position: -100% 0;
+  }
+  100% {
+    background-position: 100% 0;
+  }
+}
+@keyframes shimmer {
+  0% {
+    background-position: -1200px 0;
+  }
+  100% {
+    background-position: 1200px 0;
+  }
+}
+</style>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -8,45 +8,64 @@
     </div>
     <div class="row">
       <div class="col-sm-12">
-        <div v-for="user in users" :key="user.id" class="card mb-3">
-          <div class="card-body">
-            <div class="d-flex flex-row flex-wrap">
-              <div class="flex-grow-1">
-                <h2 class="card-title">{{ user.first_name }} {{ user.last_name }}</h2>
+        <card-loading v-if="usersLoading" class="mb-3" :shimmer-style="{ height: '75px' }" />
+        <template v-else>
+          <div v-for="user in users" :key="user.id" class="card mb-3">
+            <div class="card-body">
+              <div class="d-flex flex-row flex-wrap">
+                <div class="flex-grow-1">
+                  <h2 class="card-title">{{ user.first_name }} {{ user.last_name }}</h2>
+                </div>
+                <div class="mb-2 mb-sm-0">
+                  <button class="btn btn-sm btn-primary mr-2" type="button" @click="goToUser('display', user.id)">Display</button>
+                  <button class="btn btn-sm btn-primary mr-2" type="button" @click="goToUser('edit', user.id)">Edit</button>
+                  <button class="btn btn-sm btn-primary" type="button" @click="showDeleteModal = true; idToDelete = user.id;">Delete</button>
+                </div>
               </div>
-              <div class="mb-2 mb-sm-0">
-                <button class="btn btn-sm btn-primary mr-2" type="button" @click="goToUser('display', user.id)">Display</button>
-                <button class="btn btn-sm btn-primary mr-2" type="button" @click="goToUser('edit', user.id)">Edit</button>
-                <button class="btn btn-sm btn-primary" type="button" @click="showDeleteModal = true">Delete {{ showDeleteModal }}</button>
-              </div>
+              {{ user.address }}
             </div>
-            {{ user.address }}
           </div>
-        </div>
+        </template>
       </div>
     </div>
     <modal v-model:basedOn="showDeleteModal" title="Delete user" css-class="mw10">
-      Hello world!
+      <p>Are you sure you want to delete this user?</p>
+      <hr class="full-hr" />
+      <div class="row">
+        <div class="col">
+          <div class="float-right">
+            <button class="btn btn-sm btn-primary" @click="deleteUserAction(idToDelete)">Delete</button>
+            <button class="btn btn-sm btn-secondary ml-2" @click="showDeleteModal = false">Cancel</button>
+          </div>
+        </div>
+      </div>
     </modal>
   </div>
 </template>
 
 <script lang="ts">
-import { User } from '../api/interfaces';
-import { fetchUsers, deleteUser } from '../api/user';
+import { User } from '@/api/interfaces';
+import { fetchUsers, deleteUser } from '@/api/user';
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
+import CardLoading from '@/components/CardLoading.vue';
 
 export default {
+  components: {
+    CardLoading
+  },
   setup () {
     const router = useRouter();
     const users = ref<Array<User>>([]);
     const showDeleteModal = ref(false);
+    const idToDelete = ref(0);
+    const usersLoading = ref(true);
 
     const fetchUsersData = () => {
       return fetchUsers().then((res: User[]) => {
         // console.log(res[0].first_name);
         users.value = res;
+        usersLoading.value = false;
       });
     };
 
@@ -57,6 +76,8 @@ export default {
 
     const deleteUserAction = async (id: number) => {
       await deleteUser(id);
+      idToDelete.value = 0;
+      showDeleteModal.value = false;
       fetchUsersData();
     };
 
@@ -68,7 +89,9 @@ export default {
       users,
       goToUser,
       deleteUserAction,
-      showDeleteModal
+      showDeleteModal,
+      idToDelete,
+      usersLoading
     };
   }
 };
